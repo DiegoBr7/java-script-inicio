b9a61be1307d0c814739c3307e18ba1a0f1488b0
Diego Br <88857178+DiegoBr7@users.noreply.github.com>
2021-10-26 16:50:55 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,35 @@
-function criarAluno(){
+//function criarAluno(nome,n1,n2){
      
-return  {
+//return  {
 
-                 nome : "Igor" , 
-                 nota1 : n1 , 
-                 nota2 : n2, 
-                 media : function(){
-                 return(this.nota1+this.nota2)/ 2
-                 }
-                }
-            }
-var turma = [
-             criarAluno("Igor",9,6),
-             criarAluno("joao",7,4)
-]
-var aluno = turma[0];
-console.log(aluno);
-console.log(aluno.media());
+//                 nome : nome , 
+//                 nota1 : n1 , 
+//                 nota2 : n2, 
+//                 media : function(){
+//                 return(this.nota1+this.nota2)/ 2
+//                 }
+//                }
+//            }
+
+function aluno(nome,n1,n2)
+
+this.nome = nome;
+this.nota1 = n1;
+this.nota2 = n2;
+
+this.media = function media (){
+    return(this.nota1+this.nota2)/2;
+}
+
+
+//var turma = [
+  //           criarAluno("Igor",9,6),
+  //           criarAluno("joao",7,4)
+//]
+//var aluno = turma[2];
+
+var a = new aluno("igor",8,7);
+console.log(a)
+
+//for(var aluno of turma)
+//console.log(aluno.nome+"-"+aluno.media);
