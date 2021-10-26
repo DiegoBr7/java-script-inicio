07eb8a895146cc6d5a017548697596918c0cf695
Diego Br <88857178+DiegoBr7@users.noreply.github.com>
2021-10-26 15:47:01 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,17 @@
-function calcMedia () {
-    return (this.notas[0]+this.notas[1])/2;
+var calcMedia = function(){
+    return(this.nota1 + this.nota2)/ 2;
 }
 
-var aluno = {
-    nome: "joao",
-    notas: [5,8],
+var turma = [
+             {
+                 nome : "Igor" , 
+                 nota1 : 9 , 
+                 nota2 : 7 , 
+                 media : calcMedia
 
-     media: calcMedia
-}     
+             } 
+]
 
-var aluno1 = {
-    nome:"joao",
-    notas:[6,8],
-
-media:calcMedia
-
-}
-
-console.log(aluno.nome)
-console.log(aluno.media())
-
-console.log(aluno1.nome)
-console.log(aluno1.media())
+var aluno = turma[0];
+console.log(aluno);
+console.log(aluno.media());
