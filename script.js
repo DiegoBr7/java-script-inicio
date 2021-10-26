5de60a3c3e09e5638d364a28c18d65b9cb253eef
Diego Br <88857178+DiegoBr7@users.noreply.github.com>
2021-10-26 10:17:18 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
-var media = function (n1,n2){
 
-return (n1+n2) / 2
+//nome do aluno - nota 1 - nota 2 - media - Aprovado/ reprovado
+
+
+var nomes = ["I","J","M"];
+var notasA = [7.0,6.5,9.5];
+var notasB = [5.0,9.6,4.0];
+
+
+function media(n1,n2){
+return(n1+n2)/2
 
 }
-var m = media (5,6)
-console.log(m)
+
+for(var index in nomes){
+
+    console.log(notasA[index]+
+        "-"+
+        notasA[index]+
+        "-" +
+        notasB[index]+
+        "-"+
+        media(notasA[index],notasB[index])
+        );
+
+}
