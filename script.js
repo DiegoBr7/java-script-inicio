6c2e00520a7533e3deec48c5034f46fd53ad2ff6
Diego Br <88857178+DiegoBr7@users.noreply.github.com>
2021-10-26 14:08:01 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,42 +1,8 @@
-
-//nome do aluno - nota 1 - nota 2 - media - Aprovado/ reprovado
-
-
-var nomes = ["I","J","M"];
-var notasA = [7.0,6.5,9.5];
-var notasB = [5.0,9.6,4.0];
-
-
-function media(n1,n2){
-return(n1+n2)/2
-
+var aluno = {
+    nome: "joao",
+    notas: [2.5,5.0]
 }
+aluno.matricula=156986325;
+aluno["sobrenome"] = "oliveira"
 
-function passou (media){
-
-if(media>7){
-    resultado = "aprovado"
-}else{
-
-    return resultado = "reprovado"
-}
-
-}
-
-for(var index in nomes){
-
-var nota1 = notasA[index];
-var nota2 = notasB[index];
-
-var m = media(nota1,nota2)
-
-    console.log(nomes[index]+
-        nota1+
-        "-"+
-        nota2+
-        "-"+
-        m +
-        "-"+
-        passou(m));
-
-}
+console.log(aluno)
