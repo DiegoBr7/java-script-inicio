eb280d74db25d2a4f9b30448c8db8a2cbb202520
Diego Br <88857178+DiegoBr7@users.noreply.github.com>
2021-10-26 09:55:55 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-function saudacao () {
-
-return "ola mundo";
-
+function media (n1,n2){
+    var nota1 = n1 ;
+    var nota2 = n2;
+    var media = (nota1 + nota2)/ 2;
+    //console.log(media);
+    return media
 }
 
-var s = saudacao;
+var resultado1=media(5,6);
+var m = media
 
-console.log(s());
+var resultado2=m(2,3)
+
+console.log(resultado1 + "E" + resultado2)
