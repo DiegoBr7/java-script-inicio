b0ab7142a0d2dac3e538524c650389548236217b
Diego Br <88857178+DiegoBr7@users.noreply.github.com>
2021-10-26 09:27:22 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-function media (){
-    var nota1 = 10 ;
-    var nota2 = 8 ;
+function media (n1,n2){
+    var nota1 = n1 ;
+    var nota2 = n2;
     var media = (nota1 + nota2)/ 2;
-    console.log(media);
+//console.log(media);
+    return media
 }
 
-media();
+var resultado1=media(5,6);
+var resultado2=media(8,9);
+
+console.log(resultado1 + "E" + resultado2);
